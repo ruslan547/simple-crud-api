ad0696d0bec52da05412f8f66f77b47ffcb72b99
ruslan547 <novik.rv93@gmail.com>
2021-11-25 23:31:06 +0300

    implement server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'simple-crud-api'
-  }));
-});
+const requestListener = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end(JSON.stringify({ data: 'simple-crud-api' }));
+      break;
+    case '/person':
+      res.writeHead(200);
+      res.end('person');
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Not found' }));
+  }
+};
+
+const server = http.createServer(requestListener);
 
 server.listen(3000, (err) => {
   err ? console.log(err) : console.log(`listening port 3000`);
